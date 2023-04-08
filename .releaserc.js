5723c306f7998efa613afb68e5026e4fd0049889
canisminor1990 <i@canisminor.cc>
2023-04-08 18:06:19 +0800

    :bug: fix: action

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,17 @@
 module.exports = {
   extends: ['semantic-release-config-gitmoji'],
   branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'web.zip',
+            label: 'To install: download this file, unzip and replace ComfyUI/web',
+          },
+        ],
+      },
+    ],
+  ],
 }
