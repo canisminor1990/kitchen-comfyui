b75f670c9e452add135627814779b4a362ac34f6
canisminor1990 <i@canisminor.cc>
2023-04-26 01:29:49 +0800

    :wrench: chore: update workflow

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-clean-order'],
+  plugins: ['stylelint-order'],
   rules: {
     'declaration-block-no-duplicate-properties': null,
   },
   overrides: [
     {
       files: ['*.less', '*.css'],
-      plugins: ['stylelint-order'],
       customSyntax: 'postcss-less',
       rules: {
         'no-descending-specificity': null,
@@ -14,7 +14,6 @@ module.exports = {
     },
     {
       files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-      plugins: ['stylelint-order'],
       customSyntax: 'postcss-styled-syntax',
       rules: {
         'block-no-empty': null,
